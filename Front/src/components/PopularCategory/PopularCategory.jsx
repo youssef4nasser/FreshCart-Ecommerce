48a1b27775d21ac03815413dbb5d7456cc8ee0d3
youssef4nasser <youssef4nasser@gmail.com>
2024-05-12 20:03:28 +0300

    Update PopularCategory component to fetch category data from backend
    * Install follow-redirects version 1.15.6
    * Upgrade axios to version 1.6.8
    * Import and use axios in PopularCategory

--- Front/src/components/PopularCategory/PopularCategory.jsx
+++ Front/src/components/PopularCategory/PopularCategory.jsx
@@ -2,15 +2,26 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Container, Image, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import CategoryImage from "../../assets/CategoryImage.png"
-import CategoryImage1 from "../../assets/CategoryImage1.png"
-import CategoryImage2 from "../../assets/CategoryImage2.png"
-import CategoryImage3 from "../../assets/CategoryImage3.png"
-import CategoryImage4 from "../../assets/CategoryImage4.png"
-import CategoryImage5 from "../../assets/CategoryImage5.png"
 import styles from "./PopularCategory.module.css"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function PopularCategory() {
+  const [categoryList, setCategoryList] = useState([]);
+  // Fetch data from backend when component is mounted
+  const getCategories = async ()=>{
+    const res = await axios.get('/api/api/v1/categories');
+    if (res.data.category){
+      setCategoryList(res.data.category);
+    } else {
+      alert('Failed to load category list!')
+    }
+  }
+  
+  useEffect(() => {
+    getCategories()
+  }, [])
+  
   return <>
   <section className="py-5">
     <Container>
@@ -21,78 +32,14 @@ export default function PopularCategory() {
             <Link className="main-color text-decoration-none" href="/categories">View All <FontAwesomeIcon icon={faArrowRight} /></Link>
           </Stack>
         </Col>
-        <Col xs={4} lg={2}>
+        {categoryList.map((item)=>{ 
+          return <Col key={item._id} xs={4} lg={2}>
           <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={CategoryImage} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Accessories</h4>
+            <Image src={item.image.secure_url} alt={item.name} width={165} />
+            <h4 className={`${styles.fss} mt-3`}>{item.name}</h4>
           </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={CategoryImage1} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Smart TVs</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={CategoryImage3} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Smartwatches</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={CategoryImage4} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Headphones</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={CategoryImage5} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Laptops</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={CategoryImage2} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Mobiles & Tablets</h4>
-          </Stack>
-        </Col>
-        {/* <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={imgCategory} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Category</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={imgCategory} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Category</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={imgCategory} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Category</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={imgCategory} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Category</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={imgCategory} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Category</h4>
-          </Stack>
-        </Col>
-        <Col xs={4} lg={2}>
-          <Stack direction="vertical" className="text-center cursor-pointer  main-hover rounded-2">
-            <Image src={imgCategory} alt='category' />
-            <h4 className={`${styles.fss} mt-3`}>Category</h4>
-          </Stack>
-        </Col> */}
+        </Col> 
+        })}
       </Row>
     </Container>
   </section>
