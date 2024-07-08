e2716dd2d7efafbf2f73416990ec2c80b7974c80
Youssef AboZaid <youssef4nasser@gmail.com>
2024-07-08 21:07:45 +0300

    Update vite.config.js

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:5000',
+          target: 'https://fresh-cart-ecommerce-api.vercel.app',
           changeOrigin: true,
           secure: false,      
           ws: true,
