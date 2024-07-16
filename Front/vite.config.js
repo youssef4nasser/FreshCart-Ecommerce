eaadcf8ed31107b3f9770502feba6052280ac323
youssef4nasser <youssef4nasser@gmail.com>
2024-07-16 17:48:15 +0300

    update vite config

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
           changeOrigin: true,
           secure: false,      
           ws: true,
-          rewrite: path => path.replace(/^\/api/, '')
+          // rewrite: path => path.replace(/^\/api/, '')
         },
       },
   },
